6e09b8db124c83af80d63a96dfb965596bc5392b
Nicolas Damian <boga_nicdam@outlook.com>
2023-03-14 22:19:12 -0300

    Add Keyboard functionality

---
+++ Assets/PlayRound/Scripts/Commands/FocusInputWidget.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.PlayRound.Scripts.Commands
+{
+    public class FocusInputWidget : MonoBehaviour , ICommand
+    {
+       
+        
+
+
+        public void Start()
+        {
+            
+        }
+
+        public void Execute()
+        {
+            
+        }
+    }
+}
---
+++ Assets/PlayRound/Scripts/Keyboard/KeyboardController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+
+using UnityEngine;
+
+
+namespace Assets.PlayRound.Scripts.Keyboard
+{
+    public class KeyboardController : MonoBehaviour
+    {
+
+        public TextMeshProUGUI textField; 
+
+
+
+        private string _currentInput = "";
+
+        public void Start()
+        {
+            _currentInput = textField.text;
+            Debug.Log(_currentInput);
+        }
+
+
+        public void AddLetter(string letter)
+        {
+            _currentInput += letter; 
+            textField.text = _currentInput; 
+            Debug.Log(_currentInput);
+        }
+
+        public void EreaseLetter()
+        {
+            _currentInput = _currentInput[..^1]; // <- Borra la ultima letra del string :D
+            textField.text = _currentInput;
+        }
+
+        public void AddSpace()
+        {
+            _currentInput += " ";
+        }
+    }
+}
