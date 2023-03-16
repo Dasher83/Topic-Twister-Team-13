c251820f2cf27589b82026cc057fe62c2ab2bfe2
Mariano Lascares <99974376+MarianoLascares@users.noreply.github.com>
2023-03-15 22:04:55 -0300

    Added Timer Countdown

---
+++ Assets/PlayRound/Scripts/Timer/Timer.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI _timerText;
+    private float _numericTime;
+
+    void Start()
+    {
+        _timerText = GetComponentInChildren<TextMeshProUGUI>();
+        _numericTime = float.Parse(_timerText.text);
+    }
+
+
+    void Update()
+    {
+        CountDown();
+    }
+
+    private void CountDown()
+    {
+        if (_timerText.text == "0") return;
+        
+        _numericTime -= Time.deltaTime;
+        _timerText.text = string.Format("{0:0}", _numericTime);
+    }
+}
