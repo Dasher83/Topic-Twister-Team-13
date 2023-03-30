a7c703ddfb40e9104601fd894afcd35ed7bcc8eb
Fernando Chavez <ferman.pl@gmail.com>
2023-03-29 21:48:25 -0300

    (WIP) New Action "EvaluateAnswers"

---
+++ Assets/ResultRound/Scripts/Actions/EvaluateAnswers.cs
@@ -0,0 +1,13 @@
+using TopicTwister.Shared.Interfaces;
+
+
+namespace TopicTwister.ResultRound.Actions
+{
+    public class EvaluateAnswers : IAction
+    {
+        public void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
