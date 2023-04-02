34180857262fb8784f4a6687303c65bb722b7c5d
Mariano Lascares <99974376+MarianoLascares@users.noreply.github.com>
2023-04-02 17:55:30 -0300

    Maquetacion escena RoundResults (#1)
    * added branch
    
    * Added Scene RoundResults
    
    * Clean-up before pull-request

--- Assets/PlayRound/Scripts/StopButton/StopButton.cs
+++ Assets/PlayRound/Scripts/StopButton/StopButton.cs
@@ -7,7 +7,6 @@ namespace TopicTwister.PlayRound.Scripts.StopButton
     public class StopButton : MonoBehaviour
     {
         public UnityEvent InterruptRound = new UnityEvent();
-
         public void OnInterruptRound()
         {
             InterruptRound.Invoke();
