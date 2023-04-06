f934b5b55eda362a06f970fa3b5b68e78e5cb231
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2023-04-06 17:22:19 -0300

    Refactor 2: añadir regiones a Test_evaluate_all_empty_answers y renombramientos

--- Assets/PlayTurn/Scripts/CategoryInputList/SaveRoundAnswers.cs
+++ Assets/PlayTurn/Scripts/CategoryInputList/SaveRoundAnswers.cs
@@ -24,18 +24,18 @@ namespace TopicTwister.PlayTurn.CategoryInputList
         {
             _roundAnswersData.ClearAnswers();
 
-            RoundAnswerDTO[] roundAnswers = new RoundAnswerDTO[5];
+            TurnAnswerDTO[] turnAnswers = new TurnAnswerDTO[5];
             int index = 0;
 
             foreach (Transform childTransform in transform)
             {
                 string categoryId = childTransform.Find("Category").gameObject.GetComponent<TextMeshProUGUI>().text;
                 string userInput = childTransform.Find("UserInput").gameObject.GetComponent<TextMeshProUGUI>().text;
-                roundAnswers[index] = new RoundAnswerDTO(categoryId, userInput, order: index);
+                turnAnswers[index] = new TurnAnswerDTO(categoryId, userInput, order: index);
                 index++;
             }
 
-            _roundAnswersData.AddAnswers(roundAnswers);
+            _roundAnswersData.AddAnswers(turnAnswers);
             _loadSceneEventContainer.LoadSceneWithDelay(Scenes.TurnResultScene, 1f);
         }
     }
--- Assets/TurnResult/Tests/TurnResultEditModeTests/EvaluateAnswersUseCaseTest.cs
+++ Assets/TurnResult/Tests/TurnResultEditModeTests/EvaluateAnswersUseCaseTest.cs
@@ -14,26 +14,36 @@ public class EvaluateAnswersUseCaseTest
     [Test]
     public void Test_evaluate_all_empty_answers()
     {
+        #region -- Arrange --
         IEvaluateAnswersUseCase useCase = new EvaluateAnswersUseCase();
         char initialLetter = 'A';
-        List<RoundAnswerDTO> roundAnswers = new List<RoundAnswerDTO>()
+        List<TurnAnswerDTO> roundAnswers = new List<TurnAnswerDTO>()
         {
-            new RoundAnswerDTO("1", "", 0),
-            new RoundAnswerDTO("2", "", 1),
-            new RoundAnswerDTO("3", "", 2),
-            new RoundAnswerDTO("4", "", 3),
-            new RoundAnswerDTO("5", "", 4)
+            new TurnAnswerDTO(categoryId: "1", userInput: "", order: 0),
+            new TurnAnswerDTO(categoryId: "2", userInput: "", order: 1),
+            new TurnAnswerDTO(categoryId: "3", userInput: "", order: 2),
+            new TurnAnswerDTO(categoryId: "4", userInput: "", order: 3),
+            new TurnAnswerDTO(categoryId: "5", userInput: "", order: 4),
         };
         AnswersToEvaluateDTO answersToEvaluateStruct = new AnswersToEvaluateDTO(initialLetter, roundAnswers);
+        #endregion
 
+        #region -- Act --
         List<EvaluatedAnswerDTO> actualResult = useCase.EvaluateAnswers(answersToEvaluateStruct);
+        #endregion
 
+        #region -- Assert --
         List<EvaluatedAnswerDTO> expectedResult = new List<EvaluatedAnswerDTO>();
-        foreach (RoundAnswerDTO answer in roundAnswers)
+        foreach (TurnAnswerDTO answer in roundAnswers)
         {
-            expectedResult.Add(new EvaluatedAnswerDTO(answer.CategoryId, answer.UserInput, isCorrect: false, order: answer.Order));
+            expectedResult.Add(new EvaluatedAnswerDTO(
+                answer.CategoryId,
+                answer.UserInput,
+                isCorrect: false,
+                order: answer.Order));
         }
 
         Assert.AreEqual(expectedResult, actualResult);
+        #endregion
     }
 }
