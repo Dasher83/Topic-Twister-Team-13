ba230fc8200e34ebb1779638e0fb9d158f72d5c7
Mariano Lascares <99974376+MarianoLascares@users.noreply.github.com>
2023-04-06 16:13:50 -0300

    Rojo: Agregado test Test_evaluate_all_empty_answers

--- Assets/TurnResult/Tests/TurnResultEditModeTests/EvaluateAnswersUseCaseTest.cs
+++ Assets/TurnResult/Tests/TurnResultEditModeTests/EvaluateAnswersUseCaseTest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using TopicTwister.Shared.Structs;
 using TopicTwister.TurnResult.Shared.Interfaces;
 using TopicTwister.TurnResult.Shared.Structs;
 using TopicTwister.TurnResult.UseCases;
@@ -11,9 +12,28 @@ using UnityEngine.TestTools;
 public class EvaluateAnswersUseCaseTest
 {
     [Test]
-    public void EvaluateAnswersUseCaseTestSimplePasses()
+    public void Test_evaluate_all_empty_answers()
     {
         IEvaluateAnswersUseCase useCase = new EvaluateAnswersUseCase();
-        useCase.EvaluateAnswers(new AnswersToEvaluateStruct());
+        char initialLetter = 'A';
+        List<RoundAnswer> roundAnswers = new List<RoundAnswer>()
+        {
+            new RoundAnswer("1", "", 0),
+            new RoundAnswer("2", "", 1),
+            new RoundAnswer("3", "", 2),
+            new RoundAnswer("4", "", 3),
+            new RoundAnswer("5", "", 4)
+        };
+        AnswersToEvaluateStruct answersToEvaluateStruct = new AnswersToEvaluateStruct(initialLetter, roundAnswers);
+
+        List<EvaluatedAnswerStruct> actualResult = useCase.EvaluateAnswers(answersToEvaluateStruct);
+
+        List<EvaluatedAnswerStruct> expectedResult = new List<EvaluatedAnswerStruct>();
+        foreach (RoundAnswer answer in roundAnswers)
+        {
+            expectedResult.Add(new EvaluatedAnswerStruct(answer.CategoryId, answer.UserInput, isCorrect: false, order: answer.Order));
+        }
+
+        Assert.AreEqual(expectedResult, actualResult);
     }
 }
