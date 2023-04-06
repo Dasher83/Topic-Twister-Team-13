8b4e183ae57e84e6f12b93560c77e7939d08b09a
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2023-04-06 13:47:32 -0300

    Fix: Configuración de carpeta de tests para TurnResult

---
+++ Assets/TurnResult/Tests/TurnResultEditModeTests/EvaluateAnswersUseCaseTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+public class EvaluateAnswersUseCaseTest
+{
+    [Test]
+    public void EvaluateAnswersUseCaseTestSimplePasses()
+    {
+        Assert.Fail();
+    }
+}
