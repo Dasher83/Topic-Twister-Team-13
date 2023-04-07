454f773f095996b52d5435a00ff099330cd395ce
Mariano Lascares <99974376+MarianoLascares@users.noreply.github.com>
2023-04-07 20:56:02 -0300

    Verde: agregado nuevo test Test_evaluate_mixed_answers

--- Assets/TurnResult/Tests/TurnResultEditModeTests/EvaluateAnswersUseCaseTest.cs
+++ Assets/TurnResult/Tests/TurnResultEditModeTests/EvaluateAnswersUseCaseTest.cs
@@ -5,7 +5,9 @@ using TopicTwister.TurnResult.Shared.Interfaces;
 using TopicTwister.TurnResult.Shared.DTOs;
 using TopicTwister.TurnResult.UseCases;
 using TopicTwister.TurnResult.Repositories;
-
+using System.Collections;
+using System.Linq;
+using System;
 
 public class EvaluateAnswersUseCaseTest
 {
@@ -130,6 +132,70 @@ public class EvaluateAnswersUseCaseTest
                 Assert.AreEqual(expectedResult, actualResult);
                 #endregion
             }
+        }
+    }
+
+    [Test]
+    public void Test_evaluate_mixed_answers()
+    {
+        #region -- Arrange --
+        string alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+        Random random = new Random();
+        List<string> categoryIds = new List<string>()
+        {
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"
+        };
+        List<string> randomCategoryIds = new List<string>(categoryIds.OrderBy(x => random.Next()).Take(5));
+
+        foreach (char letter in alphabet)
+        {
+            List<TurnAnswerDTO> roundAnswers = new List<TurnAnswerDTO>()
+            {
+                new TurnAnswerDTO(categoryId: randomCategoryIds[0], userInput: $"{letter} test", order: 0),
+                new TurnAnswerDTO(categoryId: randomCategoryIds[1], userInput: "", order: 1),
+                new TurnAnswerDTO(categoryId: randomCategoryIds[2], userInput: "notTest", order: 2),
+                new TurnAnswerDTO(categoryId: randomCategoryIds[3], userInput: "", order: 3),
+                new TurnAnswerDTO(categoryId: randomCategoryIds[4], userInput: $"{letter} test", order: 4),
+            };
+            AnswersToEvaluateDTO answersToEvaluateStruct = new AnswersToEvaluateDTO(initialLetter: letter, roundAnswers);
+            #endregion
+
+            #region -- Act --
+            List<EvaluatedAnswerDTO> actualResult = _useCase.EvaluateAnswers(answersToEvaluateStruct);
+            #endregion
+
+            #region -- Assert --
+            List<EvaluatedAnswerDTO> expectedResult = new List<EvaluatedAnswerDTO>()
+            {
+                new EvaluatedAnswerDTO(
+                    category: roundAnswers[0].CategoryId,
+                    answer: roundAnswers[0].UserInput,
+                    isCorrect: true,
+                    order: roundAnswers[0].Order),
+                new EvaluatedAnswerDTO(
+                    category: roundAnswers[1].CategoryId,
+                    answer: roundAnswers[1].UserInput,
+                    isCorrect: false,
+                    order: roundAnswers[1].Order),
+                new EvaluatedAnswerDTO(
+                    category: roundAnswers[2].CategoryId,
+                    answer: roundAnswers[2].UserInput,
+                    isCorrect: false,
+                    order: roundAnswers[2].Order),
+                new EvaluatedAnswerDTO(
+                    category: roundAnswers[3].CategoryId,
+                    answer: roundAnswers[3].UserInput,
+                    isCorrect: false,
+                    order: roundAnswers[3].Order),
+                new EvaluatedAnswerDTO(
+                    category: roundAnswers[4].CategoryId,
+                    answer: roundAnswers[4].UserInput,
+                    isCorrect: true,
+                    order: roundAnswers[4].Order)
+            };
+
+            Assert.AreEqual(expectedResult, actualResult);
+            #endregion
         }
     }
 }
