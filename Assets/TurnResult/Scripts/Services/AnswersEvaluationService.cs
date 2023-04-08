3553c5f5de2e0b4fb5c23e141684a0c32f4a8159
Mariano Lascares <99974376+MarianoLascares@users.noreply.github.com>
2023-04-07 21:24:19 -0300

    [WIP] integrar AnswersEvaluationService

---
+++ Assets/TurnResult/Scripts/Services/AnswersEvaluationService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using TopicTwister.TurnResult.Shared.DTOs;
+using TopicTwister.TurnResult.Shared.Interfaces;
+
+
+namespace TopicTwister.TurnResult.Services
+{
+    public class AnswersEvaluationService : IAnswersEvaluationService
+    {
+        private readonly IEvaluateAnswersUseCase _useCase;
+
+        private AnswersEvaluationService() { }
+
+        public AnswersEvaluationService(IEvaluateAnswersUseCase evaluateAnswersUseCase) 
+        {
+            _useCase = evaluateAnswersUseCase;
+        }
+
+        public List<EvaluatedAnswerDTO> EvaluateAnswers(AnswersToEvaluateDTO answerToEvaluate)
+        {
+            return _useCase.EvaluateAnswers(answerToEvaluate);
+        }
+    }
+}
