52a1c4ae64c0c059e118bc15ddcbb0dc05bd6933
Dasher <agustinfernandezchavezc@gmail.com>
2023-05-09 14:38:09 -0300

    Esqueleto para tests de caso de uso de finalizacion de turno

---
+++ Assets/PlayTurn/Tests/PlayTurnEditModeTests/EndTurnUseCaseUnitTests.cs
@@ -0,0 +1,75 @@
+using System;
+using NUnit.Framework;
+
+
+public class EndTurnUseCaseUnitTests
+{
+    [SetUp]
+    public void SetUp() {}
+
+    [Test]
+    public void Test_ok_for_user_with_iniciative_inside_time_limit()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_ok_for_user_with_iniciative_outside_time_limit()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_ok_for_user_without_iniciative_inside_time_limit()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_ok_for_user_without_iniciative_outside_time_limit()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_fail_due_to_unknown_user()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_fail_due_to_unknown_match()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_fail_due_to_user_not_in_match()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_fail_due_to_unknown_turn()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_fail_due_already_ended_turn()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_fail_due_to_too_many_or_too_few_answers()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void Test_fail_due_to_category_mismatch()
+    {
+        throw new NotImplementedException();
+    }
+}
