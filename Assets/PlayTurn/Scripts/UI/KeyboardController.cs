689b2fc3b8af4415bf0b4ae09128d60d686094e4
Nicolas Damian <boga_nicdam@outlook.com>
2023-05-15 18:25:26 -0300

    Actualizado el teclado para que sea alphanumerico

--- Assets/PlayTurn/Scripts/UI/KeyboardController.cs
+++ Assets/PlayTurn/Scripts/UI/KeyboardController.cs
@@ -1,6 +1,7 @@
 ﻿using TopicTwister.PlayTurn.Shared.ScriptableObjects;
 using TopicTwister.Shared.ScriptableObjects;
 using UnityEngine;
+using TMPro;
 
 
 namespace TopicTwister.PlayTurn.UI
@@ -11,8 +12,14 @@ namespace TopicTwister.PlayTurn.UI
         [SerializeField] private InterruptTurnEventScriptable _interruptTurnEventContainer;
         [SerializeField] private MatchCacheScriptable _matchCacheData;
         [SerializeField] private UserInputPressedEventScriptable _userInputPressedEventContainer;
-
+        [SerializeField] private ChangeKeyboardModeEventScriptable _changeKeyBoardMode;
         private int _currentIndex;
+
+        private bool _numericMode;
+        [SerializeField] private GameObject[] _rowsAlpha;
+        [SerializeField] private GameObject _rowNumeric;
+        [SerializeField] private TextMeshProUGUI _alphaNumericText;
+        
 
         private bool _blockKeyboard;
 
@@ -24,10 +31,35 @@ namespace TopicTwister.PlayTurn.UI
         public void Start()
         {
             _currentIndex = 0;
+            _numericMode = false;
+            _rowNumeric.SetActive(false);
             _blockKeyboard = false;
             _timeOutEventContainer.TimeOut += InputEndEventHandler;
             _interruptTurnEventContainer.InterruptTurn += InputEndEventHandler;
             _userInputPressedEventContainer.OnInputPressed += CurrentIndex;
+            _changeKeyBoardMode.ChangeKeyboardMode += ChangeMode;
+        }
+
+        public void ChangeMode()
+        {
+            _numericMode = !_numericMode;
+
+            _rowNumeric.SetActive(_numericMode);
+
+            foreach (var row in _rowsAlpha)
+            {
+                row.SetActive(!_numericMode);
+            }
+
+            if (_numericMode)
+            {
+                _alphaNumericText.text = "abc..";
+            }
+            else
+            {
+                _alphaNumericText.text = "123..";    
+            }
+
         }
 
         public void AddLetter(string letter)
