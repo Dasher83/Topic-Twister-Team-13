6c3f3b5a43dfb58874bea53f8dcd2a1074e1560b
Nicolas Damian <90337058+Damian-Bogarin@users.noreply.github.com>
2023-05-18 20:36:37 -0300

    Actualizado el teclado para que sea alphanumerico (#21)

---
+++ Assets/PlayTurn/Scripts/Shared/ScriptableObjects/ChangeKeyboardModeEventScriptable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace TopicTwister.PlayTurn.Shared.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "ChangeKeyboardModeEvent", menuName = "ScriptableObjects/Events/ChangeKeyboardModeEvent")]
+    public class ChangeKeyboardModeEventScriptable : ScriptableObject
+    {
+        public Action ChangeKeyboardMode;
+    }
+}
